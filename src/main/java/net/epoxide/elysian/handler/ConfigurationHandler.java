2255fbe51304523bbefeaec8ddc48bf3cf92e68a
Darkhax <darklime@live.ca>
2015-07-17 07:42:35 -0600

    Fixed both dimensions using the same id.

--- src/main/java/net/epoxide/elysian/handler/ConfigurationHandler.java
+++ src/main/java/net/epoxide/elysian/handler/ConfigurationHandler.java
@@ -37,8 +37,8 @@ public class ConfigurationHandler {
     private void syncConfigData () {
     
         dimensionID = config.getInt("dimensionID", config.CATEGORY_GENERAL, DimensionManager.getNextFreeDimId(), -10000, 10000, "The dimension ID for the Elysian dimension. If you have succesfully launched a world with the Elysian mod once, do not change this!");
-        biomeTestID = config.getInt("biomeTestID", "Biomes", Utilities.getAvailableBiomeID(), 0, BiomeGenBase.getBiomeGenArray().length, "The biome ID for the test1 biome. If you have successfully launched a world with the Elysian mod once, do not change this!");
-        biomeTestID2 = config.getInt("biomeTestID", "Biomes", Utilities.getAvailableBiomeID(), 0, BiomeGenBase.getBiomeGenArray().length, "The biome ID for the test2 biome. If you have successfully launched a world with the Elysian mod once, do not change this!");
+        biomeTestID = config.getInt("biomeTestID1", "Biomes", Utilities.getAvailableBiomeID(), 0, BiomeGenBase.getBiomeGenArray().length, "The biome ID for the test1 biome. If you have successfully launched a world with the Elysian mod once, do not change this!");
+        biomeTestID2 = config.getInt("biomeTestID2", "Biomes", Utilities.getAvailableBiomeID(), 0, BiomeGenBase.getBiomeGenArray().length, "The biome ID for the test2 biome. If you have successfully launched a world with the Elysian mod once, do not change this!");
         
         if (config.hasChanged())
             config.save();
