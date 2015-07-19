27164238afcc8b8f5fb92ff6d9b524d2c79fe8c8
ArtixAllMighty <robbeenaxel@hotmail.com>
2015-07-19 11:13:22 +0200

    added a logger

---
+++ src/main/java/net/epoxide/elysian/LogElysian.java
@@ -0,0 +1,12 @@
+package net.epoxide.elysian;
+
+public class LogElysian {
+
+	public static void info(String s){
+		System.out.println("[INFO] "+s);
+	}
+	
+	public static void error(String s){
+		System.out.println("[ERROR] "+s);
+	}
+}
