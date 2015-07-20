ae01bb818e007a8f26ea8ff312680584edecf8fb
ArtixAllMighty <robbeenaxel@hotmail.com>
2015-07-20 16:03:58 +0200

    added the start of underwater breathing

--- src/main/java/net/epoxide/elysian/handler/ElysianEventHandler.java
+++ src/main/java/net/epoxide/elysian/handler/ElysianEventHandler.java
@@ -5,15 +5,20 @@ import java.util.Map;
 
 import net.epoxide.elysian.blocks.BlockHandler;
 import net.epoxide.elysian.items.ItemHandler;
+import net.epoxide.elysian.items.ItemTurtleArmor;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.FillBucketEvent;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.Event.Result;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
 public class ElysianEventHandler {
     
@@ -22,6 +27,7 @@ public class ElysianEventHandler {
     public ElysianEventHandler() {
     
         MinecraftForge.EVENT_BUS.register(this);
+        FMLCommonHandler.instance().bus().register(this);
         buckets.put(BlockHandler.water, ItemHandler.bucket);
     }
     
@@ -48,5 +54,33 @@ public class ElysianEventHandler {
         }
         else
             return null;
+    }
+    
+    @SubscribeEvent
+    public void stuff(PlayerTickEvent evt){
+    	EntityPlayer player = evt.player;
+    	
+    	if(isWearingFullTurtleSuite(player)){
+    		if(player.isInsideOfMaterial(Material.water)){
+    			//TODO add iff check to check if tank is full
+    			player.setAir(300);
+    			//TODO if tank is full, decrease tank air time
+    		}
+    	}
+    }
+    
+    private boolean isWearingFullTurtleSuite(EntityPlayer player){
+    	
+    	boolean set[] = new boolean[4];
+    	
+    	for(int index = 0; index <4; index++){
+    		if(player.inventory.armorItemInSlot(index) != null){
+    			Item item = player.inventory.armorItemInSlot(index).getItem();
+    			if(item != null && item instanceof ItemTurtleArmor)
+    				set[index] = true;
+    		}
+    	}
+    	
+    	return set[0] && set[1] && set[2] && set[3] ? true : false;
     }
 }
