50b7ff9ce0ea063ffd720ef349244aa3b6ed9063
Darkhax <darklime@live.ca>
2015-07-24 20:05:59 -0600

    Unimplemented the armor rendering event. This can be handled in the renderer for the armor and should not be done through events!

--- src/main/java/net/epoxide/elysian/items/ItemRunicDivingSuit.java
+++ src/main/java/net/epoxide/elysian/items/ItemRunicDivingSuit.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import net.epoxide.elysian.Elysian;
 import net.epoxide.elysian.client.model.ModelHandler;
-import net.epoxide.elysian.client.model.armor.ModelRunicDivingSuit;
 import net.epoxide.elysian.lib.Utilities;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -17,12 +15,10 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.EntityViewRenderEvent.FogColors;
 import net.minecraftforge.client.event.EntityViewRenderEvent.FogDensity;
-import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 import org.lwjgl.opengl.GL11;
@@ -32,9 +28,6 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemRunicDivingSuit extends ItemArmor {
-    
-    ResourceLocation visier_Overlay = new ResourceLocation("elysian:textures/items/armor/overlay_visier.png");
-    ResourceLocation armor_Overlay = new ResourceLocation("elysian:textures/items/armor/armor_0_overlay.png");
     
     public ItemRunicDivingSuit(int armorType) {
     
@@ -187,82 +180,6 @@ public class ItemRunicDivingSuit extends ItemArmor {
                 event.density = 0.002f;
                 GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_EXP);
                 GL11.glFogf(GL11.GL_FOG_DENSITY, 1.0f + (0.1F - (float) EnchantmentHelper.getRespiration(player) * 0.03F));
-            }
-        }
-    }
-    
-    ModelRunicDivingSuit suit = new ModelRunicDivingSuit(1.0f);
-    
-    @SubscribeEvent
-    public void onPlayerRenderArmor (RenderPlayerEvent.SetArmorModel evt) {
-    
-        if (evt.entityPlayer != null) {
-            EntityPlayer player = (EntityPlayer) evt.entityPlayer;
-            
-            ModelBiped main = evt.renderer.modelBipedMain;
-            
-            if (evt.stack != null && evt.stack.getItem().equals(ItemHandler.runicDivingHelm)) {
-                GL11.glPushMatrix();
-                GL11.glEnable(GL11.GL_BLEND);
-                Minecraft.getMinecraft().renderEngine.bindTexture(visier_Overlay);
-                GL11.glColor4f(1, 0, 1, 0.5f);
-                suit.bipedHead.render(0.0625f);
-                suit.bipedHead.rotateAngleX = main.bipedHead.rotateAngleX;
-                suit.bipedHead.rotateAngleY = main.bipedHead.rotateAngleY;
-                suit.bipedHead.rotationPointY = main.bipedHead.rotationPointY;
-                GL11.glDisable(GL11.GL_BLEND);
-                GL11.glPopMatrix();
-                
-                GL11.glPushMatrix();
-                GL11.glEnable(GL11.GL_BLEND);
-                Minecraft.getMinecraft().renderEngine.bindTexture(armor_Overlay);
-                GL11.glColor4f(1, 0, 1, 0.5f);
-                suit.bipedHead.render(0.0625f);
-                suit.bipedHead.rotateAngleX = main.bipedHead.rotateAngleX;
-                suit.bipedHead.rotateAngleY = main.bipedHead.rotateAngleY;
-                suit.bipedHead.rotationPointY = main.bipedHead.rotationPointY;
-                GL11.glDisable(GL11.GL_BLEND);
-                GL11.glPopMatrix();
-            }
-            
-            if (evt.stack != null && evt.stack.getItem().equals(ItemHandler.runicDivingSuit)) {
-                GL11.glPushMatrix();
-                GL11.glEnable(GL11.GL_BLEND);
-                Minecraft.getMinecraft().renderEngine.bindTexture(armor_Overlay);
-                GL11.glColor4f(1, 0, 1, 1f);
-                
-                suit.bipedBody.render(0.0625f);
-                suit.bipedBody.rotateAngleX = main.bipedBody.rotateAngleX;
-                suit.bipedBody.rotateAngleY = main.bipedBody.rotateAngleY;
-                suit.bipedBody.rotationPointY = main.bipedBody.rotationPointY;
-                
-                GL11.glDisable(GL11.GL_BLEND);
-                GL11.glPopMatrix();
-            }
-            
-            if (evt.stack != null && evt.stack.getItem().equals(ItemHandler.runicDivingBoots)) {
-                GL11.glPushMatrix();
-                GL11.glEnable(GL11.GL_BLEND);
-                Minecraft.getMinecraft().renderEngine.bindTexture(armor_Overlay);
-                GL11.glColor4f(1, 0, 1, 1f);
-                
-                suit.bipedRightLeg.render(0.0625f);
-                suit.bipedLeftLeg.render(0.0625f);
-                
-                suit.bipedRightLeg.rotateAngleX = main.bipedRightLeg.rotateAngleX;
-                suit.bipedRightLeg.rotateAngleY = main.bipedRightLeg.rotateAngleY;
-                suit.bipedRightLeg.rotationPointY = main.bipedRightLeg.rotationPointY;
-                suit.bipedRightLeg.rotationPointX = main.bipedRightLeg.rotationPointX;
-                suit.bipedRightLeg.rotationPointZ = main.bipedRightLeg.rotationPointZ;
-                
-                suit.bipedLeftLeg.rotateAngleX = main.bipedLeftLeg.rotateAngleX;
-                suit.bipedLeftLeg.rotateAngleY = main.bipedLeftLeg.rotateAngleY;
-                suit.bipedLeftLeg.rotationPointY = main.bipedLeftLeg.rotationPointY;
-                suit.bipedLeftLeg.rotationPointX = main.bipedLeftLeg.rotationPointX;
-                suit.bipedLeftLeg.rotationPointZ = main.bipedLeftLeg.rotationPointZ;
-                
-                GL11.glDisable(GL11.GL_BLEND);
-                GL11.glPopMatrix();
             }
         }
     }
